e8c892993ef36ddb57cbf20eecaba88f7be1a34b
wangyuxuan <wyx3707@163.com>
2018-07-25 08:50:57 +0800

    sss

---
+++ src/main/java/com/ecej/config/TestGitConfiguration.java
@@ -0,0 +1,11 @@
+package com.ecej.config;
+
+/**
+ * @ClassName TestGitConfiguration
+ * @Desc TODO
+ * @Author wyx
+ * @Date 2018/7/25 8:50
+ * @Version 1.0
+ */
+public class TestGitConfiguration {
+}
